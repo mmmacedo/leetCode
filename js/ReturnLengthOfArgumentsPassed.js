097771282ce84a45c8abfea9dbd1f98e99f43e10
Murilo Macedo <murilommacedo666@gmail.com>
2024-11-26 22:59:05 -0300

    Create ReturnLengthOfArgumentsPassed.js

---
+++ js/ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,31 @@
+/*
+Return Length of Arguments Passed
+
+Write a function argumentsLength that returns the count of arguments passed to it.
+
+Example 1:
+Input: args = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+One value was passed to the function so it should return 1.
+
+Example 2:
+Input: args = [{}, null, "3"]
+Output: 3
+Explanation: 
+argumentsLength({}, null, "3"); // 3
+Three values were passed to the function so it should return 3.
+
+Runtime 47 ms
+Beats 80.40%
+
+Analyze Complexity
+Memory 49.15 MB
+Beats 13.72%
+
+*/
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
